8f97e0785d21a8d16fd04579e63a4c60150911bb
fselmakpnr <akpinarselma1@gmail.com>
2023-05-19 18:17:54 +0300

    bazı minik düzenlemeler yaptım

--- web site/login.php
+++ web site/login.php
@@ -1,18 +1,14 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $username = $_POST["username"];
-  $password = $_POST["password"];
-  
-  $validUsername = "b221210087@sakarya.edu.tr";
-  $validPassword = "b221210087";
+if (isset($_POST['email'],$_POST['sifre'])&&($_POST['email']=='B221210087@sakarya.edu.tr')&&($_POST['sifre']=='b221210087'))
+{
+  echo("Hoşgeldin B22121087");
+  echo"<p><a href='index.html'> &lt;Anasayfaya Geri Dönün&gt;</a></p>";
 
-  if ($username == $validUsername && $password == $validPassword) {
-    
-    echo "Hoşgeldiniz \"" . $validPassword . "\"";
-  } else {
-    
-    header("Location: login.html");
-    exit;
-  }
+}
+else 
+{
+ echo("Hatalı Giriş Yaptınız!!!");
+ echo"<p><a href='login.html'>&lt;Login Sayfasına Geri Dönün&gt;</a></p>";
+
 }
 ?>
